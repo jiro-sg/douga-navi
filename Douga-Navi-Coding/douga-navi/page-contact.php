955c0a9525de12ca0cedc5e0313c6424cdd13ccd
Sakagami <osaranger@gmail.com>
2024-06-20 14:17:15 +0900

    お問い合わせフォームPC完了（フォーム設定はまだ）

--- Douga-Navi-Coding/douga-navi/page-contact.php
+++ Douga-Navi-Coding/douga-navi/page-contact.php
@@ -1,65 +1,71 @@
 <?php get_header(); ?>
-<div class="p-contact">
- <div class="p-contact__inner">
+<div class="p-contact l-contact">
+ <div class="p-contact__inner l-inner">
+  <h2 class="p-contact__title c-secTtl01">お問い合わせフォーム</h2>
+  <p class="p-contact__explain">ご相談・お見積もりは無料です。 専門スタッフが丁寧にご用件をお伺いいたします。お気軽にご相談ください</p>
+
   <form class="p-contact__form p-form" action="">
-   <dl class="p-form__wrap">
-    <dt class="p-form__label">名前<span>必須</span></dt>
-    <dd class="p-form__input p-form-input">
-     <input type="text">
-    </dd>
-   </dl>
-   <dl class="p-form__wrap">
-    <dt class="p-form__label">電話番号<span>必須</span></dt>
-    <dd class="p-form__input p-form-input">
-     <input type="tel">
-    </dd>
-   </dl>
-   <dl class="p-form__wrap">
-    <dt class="p-form__label">メールアドレス<span>必須</span></dt>
-    <dd class="p-form__input p-form-input">
-     <input type="email">
-    </dd>
-   </dl>
-   <dl class="p-form__wrap">
-    <dt class="p-form__label">住所</dt>
-    <dd class="p-form__select p-form-select">
-     <select>
-      <option hidden>選択してください</option>
-      <option value="東京">東京</option>
-      <option value="大阪">大阪</option>
-      <option value="福岡">福岡</option>
-      <option value="その他">その他</option>
-     </select>
-    </dd>
-   </dl>
-   <dl class="p-form__wrap">
-    <dt class="p-form__label">カテゴリ</dt>
-    <dd class="p-form__radio p-form-radio">
-     <label><input type="radio" name="category" value="ホームページ制作"><span>ホームページ制作</span></label>
-     <label><input type="radio" name="category" value="SNS運用"><span>SNS運用</span></label>
-     <label><input type="radio" name="category" value="その他"><span>その他</span></label>
-    </dd>
-   </dl>
-   <dl class="p-form__wrap">
-    <dt class="p-form__label">お持ちのSNS</dt>
-    <dd class="p-form__checkbox p-form-checkbox">
-     <label><input type="checkbox" name="sns" value="Twitter"><span>Twitter</span></label>
-     <label><input type="checkbox" name="sns" value="Instagram"><span>Instagram</span></label>
-     <label><input type="checkbox" name="sns" value="Facebook"><span>Facebook</span></label>
-     <label><input type="checkbox" name="sns" value="YouTube"><span>YouTube</span></label>
-     <label><input type="checkbox" name="sns" value="その他"><span>その他</span></label>
-    </dd>
-   </dl>
-   <dl class="p-form__wrap p-form__wrap--textarea">
-    <dt class="p-form__label">お問い合わせ内容<span>必須</span></dt>
-    <dd class="p-form__textarea p-form-textarea">
-     <textarea name="message"></textarea>
-    </dd>
-   </dl>
-   <div class="p-form__submit p-form-submit">
-    <input type="submit" value="送信">
+
+   <div class="p-form__inner">
+    <dl class="p-form__wrap">
+     <dt class="p-form__label">お名前<span>（必須）</span></dt>
+     <dd class="p-form__input p-form-input">
+      <input type="text" placeholder="例）動画太郎">
+     </dd>
+    </dl>
+    <dl class="p-form__wrap">
+     <dt class="p-form__label">貴社名</dt>
+     <dd class="p-form__input p-form-input">
+      <input type="text" placeholder="例）xxx@douganavi.biz">
+     </dd>
+    </dl>
+    <dl class="p-form__wrap">
+     <dt class="p-form__label">電話番号<span>（必須）</span></dt>
+     <dd class="p-form__input p-form-input">
+      <input type="tel" placeholder="例）09012345678">
+     </dd>
+    </dl>
+    <dl class="p-form__wrap">
+     <dt class="p-form__label">メールアドレス<span>（必須）</span></dt>
+     <dd class="p-form__input p-form-input">
+      <input type="email" placeholder="例）xxx@douganavi.biz">
+     </dd>
+    </dl>
+    <dl class="p-form__wrap">
+     <dt class="p-form__label">メールマガジン</dt>
+     <dd class="p-form__checkbox p-form-checkbox">
+      <label><input type="checkbox" name="sns" value=""><span>動画ナビからのお役立ち情報を希望する</span></label>
+     </dd>
+    </dl>
+    <dl class="p-form__wrap">
+     <dt class="p-form__label">相談内容</dt>
+     <dd class="p-form__checkbox p-form-checkbox">
+      <label><input type="checkbox" name="sns" value=""><span>動画制作について</span></label>
+      <label><input type="checkbox" name="sns" value=""><span>動画を使ったマーケティングについて</span></label>
+      <label><input type="checkbox" name="sns" value=""><span>その他</span></label>
+     </dd>
+    </dl>
+    <dl class="p-form__wrap p-form__wrap--textarea">
+     <dt class="p-form__label">備考</dt>
+     <dd class="p-form__textarea p-form-textarea">
+      <textarea name="message"></textarea>
+     </dd>
+    </dl>
+    <div class="p-form__wrap p-form__wrap--confirm">
+     <div class="p-form__checkbox p-form-checkbox">
+      <label><input type="checkbox" name="sns" value=""><span>入力内容に問題がないかのご確認</span></label>
+     </div>
+    </div>
+    <div class="p-form__submit p-form-submit">
+     <input type="submit" value="お問い合わせ内容を送信する">
+    </div>
    </div>
+   <p class="p-form__policy">
+    <a href=""><span>個人情報保護方針</span>について同意したものとみなされます</a>
+   </p>
   </form>
+
+
  </div>
 </div>
 

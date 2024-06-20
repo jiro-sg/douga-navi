81bb2296231c4f3e546a8adb3dfbc7ba5805594b
WoodBell <woodbell.jpb@gmail.com>
2024-06-20 15:24:44 +0900

    「動画制作ナビとは」ページの「動画制作事例」セクションまでコーディング完了（PCのみ）

---
+++ Douga-Navi-Coding/douga-navi/page-about.php
@@ -0,0 +1,179 @@
+<?php get_header(); ?>
+
+<div class="p-about">
+ <section class="p-about__fv p-aboutFv">
+  <picture>
+   <source srcset='<?php echo get_template_directory_uri() ?>/assets/images/aboutFV_pc.jpg' media='(min-width: 768px)'>
+   <img src='#' alt='課題解決から納品まで、ワンストップでプロが伴走！' width='1279' height='474'>
+  </picture>
+
+  <div class="p-aboutFv__sntnc">
+   <h1 class="p-aboutFv__ttl"><span class="p-aboutFv__ttl--blue">課題解決</span>から<span class="p-aboutFv__ttl--blue">納品</span>まで、<br><span class="p-aboutFv__ttl--orangeLine">ワンストップでプロが伴走！</span></h1>
+   <p class="p-aboutFv__lead">集客課題解決もご相談ください</p>
+
+   <div class="p-aboutFv__btn">
+    <a href="#">まずは無料相談してみる</a>
+   </div>
+  </div>
+ </section>
+ <section class="p-about__navi p-aboutNavi">
+  <div class="l-inner">
+   <h2 class="p-aboutNavi__ttl">動画製作ナビとは？</h2>
+   <div class="p-aboutNavi__txtWrppr">
+
+    <p class="p-aboutNavi__txt">動画制作ナビは現役クリエイティブエージェンシー所属の専任ディレクターが、お客様の課題解決に向けて最適なプランをご提案し、厳選されたプロのパートナーと連携して高品質な動画を制作いたします。</p>
+    <p class="p-aboutNavi__txt">専任ディレクターが窓口となり、制作進行から納品までスムーズなコミュニケーションを実現。明瞭な料金設定と効率的な制作体制で、安心してスピーディーに動画制作を依頼できます。</p>
+    <p class="p-aboutNavi__txt">結婚式やお祝い事、ペット動画などライフイベントにおける動画制作も対応しております！動画動画制作に関するお悩みは、お気軽に【動画制作ナビ】へご相談ください！</p>
+   </div>
+  </div>
+ </section>
+
+ <section class="p-about__prblm p-aboutPrblm">
+  <div class="l-inner">
+
+   <h2 class="p-aboutPrblm__ttl"><span>動画制作のこんなお悩み、ありませんか？</span></h2>
+
+   <div class="p-aboutPrblm__cardWrppr">
+
+    <div class="p-aboutPrblm__cardBox">
+     <h3 class="p-aboutPrblm__cardTtl">どのように依頼をしたら<br>よいか分からない</h3>
+     <figure class="p-aboutPrblm__cardImg">
+      <img src="<?php echo get_template_directory_uri() ?>/assets/images/ico_trouble01.webp" alt="どのように依頼をしたらよいか分からない">
+     </figure>
+     <p class="p-aboutPrblm__cardTxt">初めて動画制作を行うのでどのように進めればよいか分からない。</p>
+    </div>
+
+    <div class="p-aboutPrblm__cardBox">
+     <h3 class="p-aboutPrblm__cardTtl">動画制作のコストを<br>抑えたい</h3>
+     <figure class="p-aboutPrblm__cardImg">
+      <img src="<?php echo get_template_directory_uri() ?>/assets/images/ico_trouble02.webp" alt="動画制作のコストを抑えたい">
+     </figure>
+     <p class="p-aboutPrblm__cardTxt">現在のコストが適切か分からない。<br>パフォーマンスは落としたくない。</p>
+    </div>
+
+    <div class="p-aboutPrblm__cardBox">
+     <h3 class="p-aboutPrblm__cardTtl">動画を活用した<br>マーケティングを行いたい</h3>
+     <figure class="p-aboutPrblm__cardImg">
+      <img src="<?php echo get_template_directory_uri() ?>/assets/images/ico_trouble03.webp" alt="動画を活用したマーケティングを行いたい">
+     </figure>
+     <p class="p-aboutPrblm__cardTxt">動画を活用したマーケティングで集客効率を上げたい。</p>
+    </div>
+   </div>
+  </div>
+ </section>
+
+ <section class="p-about__result p-aboutRslt">
+  <div class="l-inner">
+
+   <h2 class="p-aboutRslt__ttl"><span>そのお悩み、動画制作ナビで全て解決できます！</span></h2>
+   <p class="p-aboutRslt__lead">動画制作ナビが選ばれる理由</p>
+
+
+   <div class="p-aboutRslt__cardWrppr">
+
+    <div class="p-aboutRslt__cardBox c-card01">
+
+     <div class="c-card01__sntnc">
+      <p class="c-card01__num">POINT01</p>
+      <h3 class="c-card01__ttl">初めてでも安心！専任コンサルタントが<br>ヒアリング対応</h3>
+      <p class="c-card01__txt">動画制作の依頼は明確なビジョンと目標が重要です。動画の用途や目的をヒアリングさせていただき、イメージされている動画制作を行います。具体的なイメージが出来ていない場合もイメージを具体化させるためのご提案を致します。</p>
+     </div>
+
+     <figure class="c-card01__img">
+      <img src="<?php echo get_template_directory_uri() ?>/assets/images/img_result01-pc.jpg" alt="初めてでも安心！専任コンサルタントがヒアリング対応" width="313" height="177">
+     </figure>
+
+    </div>
+
+    <div class="p-aboutRslt__cardBox c-card01">
+
+     <div class="c-card01__sntnc">
+      <p class="c-card01__num">POINT02</p>
+      <h3 class="c-card01__ttl">パフォーマンスを落とさない適切なコスト削減</h3>
+      <p class="c-card01__txt">単に動画の制作を行うのではなく、動画の用途や目的を理解させていただいたうえで動画制作を行います。目的に応じて、削減できる制作コストを算出致しますので、適切なコスト削減の提案が可能です。</p>
+     </div>
+
+     <figure class="c-card01__img">
+      <img src="<?php echo get_template_directory_uri() ?>/assets/images/img_result02-pc.jpg" alt="パフォーマンスを落とさない適切なコスト削減" width="313" height="177">
+     </figure>
+
+    </div>
+
+    <div class="p-aboutRslt__cardBox c-card01">
+
+     <div class="c-card01__sntnc">
+      <p class="c-card01__num">POINT03</p>
+      <h3 class="c-card01__ttl">動画を使ったマーケティング施策もご相談ください</h3>
+      <p class="c-card01__txt">累計15,000社以上のマーケティング・集客支援実績があり、目的から逆算した動画制作のご提案を強みとしています。企業規模、業種、用途、オンラインサービス・店舗型サービスを問わずあらゆるシーンごとで動画を活用した集客パフォーマンス改善のご提案を致します。</p>
+     </div>
+
+     <figure class="c-card01__img">
+      <img src="<?php echo get_template_directory_uri() ?>/assets/images/img_result03-pc.jpg" alt="動画を使ったマーケティング施策もご相談ください" width="313" height="177">
+     </figure>
+
+    </div>
+   </div>
+  </div>
+ </section>
+ <section class="p-about__exmpl p-aboutExmpl">
+  <div class="l-inner">
+
+   <h2 class="p-aboutExmpl__ttl">動画制作事例</h2>
+
+   <div class="p-aboutExmpl__cardWrppr">
+
+    <div class="p-aboutExmpl__movieCard">
+     <figure>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/bjmBJ1Fl0cs?si=H-2LtWB8we2sSyTU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     </figure>
+     <p class="p-aboutExmpl__cardTxt">動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります</p>
+    </div>
+
+    <div class="p-aboutExmpl__movieCard">
+     <figure>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/bjmBJ1Fl0cs?si=H-2LtWB8we2sSyTU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     </figure>
+     <p class="p-aboutExmpl__cardTxt">動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります</p>
+    </div>
+
+    <div class="p-aboutExmpl__movieCard">
+     <figure>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/bjmBJ1Fl0cs?si=H-2LtWB8we2sSyTU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     </figure>
+     <p class="p-aboutExmpl__cardTxt">動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります</p>
+    </div>
+
+    <div class="p-aboutExmpl__movieCard">
+     <figure>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/bjmBJ1Fl0cs?si=H-2LtWB8we2sSyTU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     </figure>
+     <p class="p-aboutExmpl__cardTxt">動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります</p>
+    </div>
+
+    <div class="p-aboutExmpl__movieCard">
+     <figure>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/bjmBJ1Fl0cs?si=H-2LtWB8we2sSyTU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     </figure>
+     <p class="p-aboutExmpl__cardTxt">動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります</p>
+    </div>
+
+    <div class="p-aboutExmpl__movieCard">
+     <figure>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/bjmBJ1Fl0cs?si=H-2LtWB8we2sSyTU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     </figure>
+     <p class="p-aboutExmpl__cardTxt">動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります動画の説明文が入ります</p>
+    </div>
+
+   </div>
+
+  </div>
+ </section>
+
+section.p-about
+
+
+
+</div>
+
+
+<?php get_footer(); ?>

7ac2125308a8f95f7122ccbfa6ca9f1f77c132c4
Sakagami <osaranger@gmail.com>
2024-06-27 17:00:51 +0900

    送信完了ページ、404ページを調整しました。

--- Douga-Navi-Coding/douga-navi/taxonomy-faq_menu.php
+++ Douga-Navi-Coding/douga-navi/taxonomy-faq_menu.php
@@ -1,33 +1,33 @@
 <?php get_header(); ?>
 <main class="l-main">
- <!-- パンくずリスト -->
- <?php get_template_part('_inc/breadcrumb'); ?>
- <!-- // -->
+  <!-- パンくずリスト -->
+  <?php get_template_part('_inc/breadcrumb'); ?>
+  <!-- // -->
 
- <div class="p-faq-category l-faq-category">
-  <div class="l-inner">
-   <div class="p-faq-category__wrap">
-    <h2 class="p-faq-category__title c-secTtl02">カテゴリごとによくある<br class="u-mobile">ご質問を検索する</h2>
-    <ul class="p-faq-category__items">
-
-
-     <?php get_template_part('_inc/faq_category'); ?>
+  <div class="p-faq-category l-faq-category">
+    <div class="l-inner">
+      <div class="p-faq-category__wrap">
+        <h2 class="p-faq-category__title c-secTtl02">カテゴリごとによくある<br class="u-mobile">ご質問を検索する</h2>
+        <ul class="p-faq-category__items">
 
 
-    </ul>
-   </div>
+          <?php get_template_part('_inc/faq_category'); ?>
+
+
+        </ul>
+      </div>
+    </div>
+
   </div>
 
- </div>
+  <section class="p-faq l-faq">
 
- <section class="p-faq l-faq">
-
-  <div class="p-faq__inner l-inner">
-   <h2 class="p-faq__title c-secTtl01">よくある質問</h2>
-   <div class="p-faq__wrap">
+    <div class="p-faq__inner l-inner">
+      <h2 class="p-faq__title c-secTtl01">よくある質問</h2>
+      <div class="p-faq__wrap">
 
 
-    <?php
+        <?php
         if (wp_is_mobile()) {
           $num = -1; // スマホの表示数(全件は-1)
         } else {
@@ -52,39 +52,39 @@
         ?>
 
 
-    <dl class="p-faq__item">
-     <dt class="p-faq__question">
-      <?php the_title(); ?>
-     </dt>
-     <dd class="p-faq__answer">
-      <?php $value = get_post_meta($post->ID, 'answer', true); ?>
-      <?php if (!empty($value)) : ?>
-      <?php the_field('answer'); ?>
-      <?php else : ?>
+            <dl class="p-faq__item">
+              <dt class="p-faq__question">
+                <?php the_title(); ?>
+              </dt>
+              <dd class="p-faq__answer">
+                <?php $value = get_post_meta($post->ID, 'answer', true); ?>
+                <?php if (!empty($value)) : ?>
+                  <?php the_field('answer'); ?>
+                <?php else : ?>
 
-      <?php endif; ?>
-     </dd>
+                <?php endif; ?>
+              </dd>
 
-    </dl>
-    <?php endwhile;
+            </dl>
+          <?php endwhile;
         else : ?>
-    <?php endif ?>
-    <?php wp_reset_postdata(); ?>
+        <?php endif ?>
+        <?php wp_reset_postdata(); ?>
 
-   </div>
-  </div>
-  <div class="p-faq__banner c-ctaBanner">
-   <p class="c-ctaBanner__txt">動画制作・動画集客に関することはお気軽にご相談ください。<br>専任スタッフがすぐにご連絡いたします。
-   </p>
-   <div class="c-ctaBanner__btn">
-    <a href="<?php echo esc_url(home_url('/contact/')); ?>">まずは無料相談してみる</a>
-   </div>
-  </div>
-  <div class="p-faq__return c-returnBtn">
-   <a href="<?php echo esc_url(home_url('/')); ?>">ホームへ戻る</a>
-  </div>
+      </div>
+    </div>
+    <div class="p-faq__banner c-ctaBanner">
+      <p class="c-ctaBanner__txt">動画制作・動画集客に関することはお気軽にご相談ください。<br>専任スタッフがすぐに<br class="u-mobile">ご連絡いたします。
+      </p>
+      <div class="c-ctaBanner__btn">
+        <a href="<?php echo esc_url(home_url('/contact/')); ?>">まずは無料相談してみる</a>
+      </div>
+    </div>
+    <div class="p-faq__return c-returnBtn">
+      <a href="<?php echo esc_url(home_url('/')); ?>">ホームへ戻る</a>
+    </div>
 
- </section>
+  </section>
 </main>
 
 
